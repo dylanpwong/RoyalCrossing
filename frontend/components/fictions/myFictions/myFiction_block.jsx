234a434b5e91e6dd1c2fdad24faa17df9a38cc03
dpwong <dylanpwong@gmail.com>
2020-04-10 12:15:41 -0400

    before lunch push

--- frontend/components/fictions/myFictions/myFiction_block.jsx
+++ frontend/components/fictions/myFictions/myFiction_block.jsx
@@ -13,7 +13,23 @@ class MyFictionBlock extends React.Component{
             <div>
                 <div></div> {/*picture*/}
                 <div>
-                    <h4>{this.props.story.title}</h4>
+                    <h3>{this.props.story.title}</h3>
+
+                    <div> {/* the favorites/follows/ratings comments view*/}
+                        <div>
+                            <p>10</p> {/*placeHolder*/}
+                            <p>favorites</p>
+                        </div>
+                    </div>
+
+                    <nav>{/*links to show pages, and add chapter*/}
+                        <ul>
+                            <li>Page</li>
+                            <li>DashBoard</li>
+                            <li>Add Chapter</li>
+                        </ul>
+                    </nav>
+
                 </div>
             </div>
             </>
